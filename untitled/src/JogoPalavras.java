a46faa5c54ff6c07025ea2b65ad12766b5a8356a
JairCologneseJunior <102156480+JairCologneseJunior@users.noreply.github.com>
2023-12-02 22:31:01 -0300

    Add files via upload

---
+++ untitled/src/JogoPalavras.java
@@ -0,0 +1,68 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class JogoPalavras {
+    private static final int TEMPO_LIMITE_SEGUNDOS = 60;
+    private static final int TAMANHO_MINIMO_PALAVRA = 4;
+    private static final String DICIONARIO_ARQUIVO = "/br-utf8.txt";
+
+    private final Set<String> palavrasCorretas;
+    private final char letraSorteada;
+
+    public JogoPalavras() {
+        this.palavrasCorretas = new HashSet<>();
+        this.letraSorteada = sortearLetra();
+    }
+
+    public void jogar() {
+        Scanner scanner = new Scanner(System.in);
+        long tempoInicio = System.currentTimeMillis();
+
+        while (true) {
+            System.out.printf("Digite uma palavra que comece com a letra '%c': ", letraSorteada);
+            String palavra = scanner.nextLine().trim().toLowerCase();
+
+            if (palavra.length() < TAMANHO_MINIMO_PALAVRA) {
+                System.out.println("A palavra deve ter pelo menos 4 letras.");
+                continue;
+            }
+
+            if (validarPalavra(palavra)) {
+                palavrasCorretas.add(palavra);
+            } else {
+                System.out.println("Palavra inválida ou repetida.");
+            }
+
+            if (System.currentTimeMillis() - tempoInicio > TEMPO_LIMITE_SEGUNDOS * 1000) {
+                break;
+            }
+        }
+
+        System.out.printf("Você acertou %d palavras:\n", palavrasCorretas.size());
+        for (String palavra : palavrasCorretas) {
+            System.out.println(palavra);
+        }
+    }
+
+    private char sortearLetra() {
+        String alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        int indice = (int) (Math.random() * alfabeto.length());
+        return alfabeto.charAt(indice);
+    }
+
+    private boolean validarPalavra(String palavra) {
+        try (Scanner fileScanner = new Scanner(getClass().getResourceAsStream(DICIONARIO_ARQUIVO))) {
+            while (fileScanner.hasNextLine()) {
+                String linha = fileScanner.nextLine().trim().toLowerCase();
+                if (linha.equals(palavra)) {
+                    return true;
+                }
+            }
+        } catch (NullPointerException e) {
+            System.err.println("Arquivo de dicionário não encontrado.");
+        }
+
+        return false;
+    }
+}
---
+++ untitled/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        JogoPalavras jogo = new JogoPalavras();
+        jogo.jogar();
+    }
+}
